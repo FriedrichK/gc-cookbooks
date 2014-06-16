15961ec046fcd0711ef7bc90a4ad1bd6ccd17fe4
FriedrichK <friedrich@cartogami.com>
2014-06-16 12:05:44 +0200

    added gc-specific python cookbook

---
+++ gc-python/attributes/default.rb
@@ -0,0 +1,2 @@
+default['python']['install_method'] = 'source'
+default['python']['min_version'] = '2.7.3'
---
+++ gc-python/metadata.rb
@@ -0,0 +1,10 @@
+name              "gc-python"
+maintainer        "Friedrich Kauder"
+maintainer_email  "fkauder@gmail.com"
+license           "Apache 2.0"
+description       "Installs Python for the Gamecoach application"
+version           "0.0.1"
+
+#depends           "python"
+
+recipe "gc-python", "Installs Python for the Gamecoach application"
