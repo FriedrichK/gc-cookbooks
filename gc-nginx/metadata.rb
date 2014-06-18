42046d02f6a07cbc04203c1f315b0bb8eaa45471
FriedrichK <friedrich@cartogami.com>
2014-06-18 19:21:28 +0200

    added wrapper cookbook for nginx

---
+++ gc-nginx/metadata.rb
@@ -0,0 +1,10 @@
+name              "gc-nginx"
+maintainer        "Friedrich Kauder"
+maintainer_email  "fkauder@gmail.com"
+license           "Apache 2.0"
+description       "Installs nginx for the Gamecoach application"
+version           "0.0.1"
+
+depends           "nginx"
+
+recipe "gc-python", "Installs nginx for the Gamecoach application"
