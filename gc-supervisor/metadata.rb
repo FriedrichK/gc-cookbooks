39a207cdaa92e3386c0744a8eb677cbd8e4bc5f8
FriedrichK <friedrich@cartogami.com>
2014-06-18 19:22:09 +0200

    added wrapper cookbook for supervisor

---
+++ gc-supervisor/metadata.rb
@@ -0,0 +1,10 @@
+name              "gc-supervisor"
+maintainer        "Friedrich Kauder"
+maintainer_email  "fkauder@gmail.com"
+license           "Apache 2.0"
+description       "Installs supervisor for the Gamecoach application"
+version           "0.0.1"
+
+depends           "supervisor"
+
+recipe "gc-python", "Installs supervisor for the Gamecoach application"
---
+++ gc-supervisor/recipes/default.rb
@@ -0,0 +1,12 @@
+template "/etc/rc.d/init.d/supervisord" do
+  source "supervisor.erb"
+  owner "root"
+  group "root"
+  mode 00755
+end
+
+bash "chkconfig --level 345 supervisord on" do
+  user "root"
+end
+
+include_recipe 'supervisor'
