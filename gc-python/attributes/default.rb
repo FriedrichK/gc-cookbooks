2637ee43933a46cf87cc02048abe8ab7bb804dcc
FriedrichK <friedrich@cartogami.com>
2014-06-18 12:01:26 +0200

    made Python wrapper cookbook work

--- gc-python/attributes/default.rb
+++ gc-python/attributes/default.rb
@@ -1,2 +1,3 @@
 default['python']['install_method'] = 'source'
 default['python']['min_version'] = '2.7.3'
+default['python']['make_options'] = %W{altinstall}
--- gc-python/metadata.rb
+++ gc-python/metadata.rb
@@ -5,6 +5,6 @@ license           "Apache 2.0"
 description       "Installs Python for the Gamecoach application"
 version           "0.0.1"
 
-#depends           "python"
+depends           "python"
 
 recipe "gc-python", "Installs Python for the Gamecoach application"
