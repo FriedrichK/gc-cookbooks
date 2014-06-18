182ac333bebc8417f5e9975d9fbdd24197c34ced
FriedrichK <friedrich@cartogami.com>
2014-06-18 19:21:47 +0200

    added wrapper cookbook for postgresql

---
+++ gc-postgresql/metadata.rb
@@ -0,0 +1,10 @@
+name              "gc-nginx"
+maintainer        "Friedrich Kauder"
+maintainer_email  "fkauder@gmail.com"
+license           "Apache 2.0"
+description       "Installs PostGreSQL for the Gamecoach application"
+version           "0.0.1"
+
+depends           "postgresql"
+
+recipe "gc-python", "Installs PostGreSQL for the Gamecoach application"
---
+++ gc-postgresql/recipes/server.rb
@@ -0,0 +1,4 @@
+package "postgresql93-libs" do	
+end
+
+include_recipe 'postgresql::server'
